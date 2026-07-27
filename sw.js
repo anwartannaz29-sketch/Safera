90dbf39a6b84ba0e11754bead8fb39387125de25
AnwarTannaz <anwartannaz29@gmail.com>
2026-07-27 09:01:23 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'kas2026-v1';
+const ASSETS = ['/', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
